27526f4e3dbcd46c4503c606c7630c8ac5882909
sharon <sapot@safaricom.co.ke>
2023-10-19 21:14:27 +0300

    Add code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the 'main' element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new 'h1' element with id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the inner HTML of the 'newHeader' element
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// Append 'newHeader' to the body
+document.body.appendChild(newHeader);
